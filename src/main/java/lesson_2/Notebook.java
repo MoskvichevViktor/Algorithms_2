74975b355a8f60d89b05bfb21a10ad34aba3affe
MoskvichevViktor <beldetal@yandex.ru>
2021-10-11 22:13:56 +0300

    finish

---
+++ src/main/java/lesson_2/Notebook.java
@@ -0,0 +1,29 @@
+package lesson_2;
+
+import lombok.Value;
+import java.math.BigDecimal;
+
+@Value
+public class Notebook implements Comparable<Notebook> {
+    BigDecimal price;
+    int memory;
+    BrandName brandName;
+
+    public  enum BrandName {
+        LENUVO, ASOS, MACNOTE, ESER, XAMIOU
+    }
+
+    @Override
+    public  int compareTo(Notebook n){
+        int result = price.compareTo((n.getPrice()));
+        if (result == 0) {
+            result = Integer.compare(memory, n.getMemory());
+            if (result == 0) {
+                return brandName.compareTo(n.getBrandName());
+            }
+            return result;
+        }
+        return result;
+    }
+
+}
